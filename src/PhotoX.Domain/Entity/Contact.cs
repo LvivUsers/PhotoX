99d8ad1dbd51171a842e66f0a929ab9445776ae7
sidlovskyy <sidlovskyy@gmail.com>
2011-11-07 17:04:48 +0200

    1. Text duplication 3

--- src/PhotoX.Domain/Entity/Contact.cs
+++ src/PhotoX.Domain/Entity/Contact.cs
@@ -5,6 +5,6 @@ namespace PhotoX.Domain.Entity
     public class Contact : DomainEntity
     {
         public string Email { get; set; }
-        public IEnumerable<Phone> Phones { get; set; }        
+        public IEnumerable<Phone> Phones { get; set; }
     }
 }
