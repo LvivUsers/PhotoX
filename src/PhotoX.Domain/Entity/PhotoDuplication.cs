6fe4a3dea21d0fd9f833148d513820e8ce61c9ec
sidlovskyy <sidlovskyy@gmail.com>
2011-11-07 16:15:00 +0200

    1. Added duplication  test 2.

---
+++ src/PhotoX.Domain/Entity/PhotoDuplication.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PhotoX.Domain.Entity
+{    
+    public class PhotoDuplication : DomainEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public byte[] Image { get; set; }
+    }
+}
